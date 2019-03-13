4a18b53075c74821c38321c9310e5f3ab6f719e4
zhangqiang <57003623@qq.com>
2019-03-13 11:13:17 +0800

    增加我的信息页面

---
+++ miniprogram/pages/info/info.js
@@ -0,0 +1,79 @@
+// miniprogram/pages/info/info.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    steps: [
+      {
+        text: '手机认证'
+      },
+      {
+        text: '证件上传'
+       
+      },
+      {
+        text: '认证完成'
+      }
+    ],
+    active:0
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+      console.log("active的值"+this.data.action)
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
